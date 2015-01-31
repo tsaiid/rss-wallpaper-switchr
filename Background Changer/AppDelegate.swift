2faa0e5a707b1dc3481f6e4394898626af264c8e
I-Ta Tsai <ittsai@gmail.com>
2015-01-31 18:53:29 +0800

    Initial Commit

---
+++ Background Changer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Background Changer
+//
+//  Created by I-Ta Tsai on 2015/1/31.
+//  Copyright (c) 2015年 I-Ta Tsai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Background Changer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Background Changer
+//
+//  Created by I-Ta Tsai on 2015/1/31.
+//  Copyright (c) 2015年 I-Ta Tsai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
