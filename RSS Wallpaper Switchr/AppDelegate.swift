f3ef662b2bfabcec15813eafbf2d6f6cc4765319
I-Ta Tsai@MBPR <ittsai@gmail.com>
2015-06-09 14:16:02 +0800

    add observer for screenlock. deactivate timer when locked and reactivate when unlocked.

--- RSS Wallpaper Switchr/AppDelegate.swift
+++ RSS Wallpaper Switchr/AppDelegate.swift
@@ -324,6 +324,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // App init
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
 
@@ -331,11 +333,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             window.makeKeyAndOrderFront(self)
         #endif
 
-        // by notification, trigger switcher when space changes
+        // for notification of space changes
         NSWorkspace.sharedWorkspace().notificationCenter.addObserver(self,
             selector: "changeDesktopAfterSpaceDidChange:",
             name: NSWorkspaceActiveSpaceDidChangeNotification,
             object: NSWorkspace.sharedWorkspace())
+
+        // for notification of screensavor and screen lock
+        /*
+            // do not need to handle with screensaver, it also trigger screenIsLocked and screenIsUnlocked
+            name: "com.apple.screensaver.didstart",
+            name: "com.apple.screensaver.didstop",
+        */
+
+        NSDistributedNotificationCenter.defaultCenter().addObserver(self,
+            selector: "screenLockHandler:",
+            name: "com.apple.screenIsLocked",
+            object: nil)
+
+        NSDistributedNotificationCenter.defaultCenter().addObserver(self,
+            selector: "screenLockHandler:",
+            name: "com.apple.screenIsUnlocked",
+            object: nil)
 
         updateSwitchTimer()
     }
@@ -415,6 +434,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func changeDesktopAfterSpaceDidChange(aNotification: NSNotification) {
         println("space changed")
         //setDesktopBackgrounds()
+    }
+    
+    func screenLockHandler(aNotification: NSNotification) {
+        println("screen status: \(aNotification.name)")
+        if aNotification.name == "com.apple.screenIsLocked" {
+            stopSwitchTimer()
+        } else {
+            updateSwitchTimer()
+        }
     }
 }
 
