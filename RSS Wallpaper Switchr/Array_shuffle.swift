7cecef7dde3446c67e5e5fc2c1ed1e46caf3b281
I-Ta Tsai <ittsai@gmail.com>
2015-06-10 20:07:58 +0800

    extend Array to have shuffle function.

---
+++ RSS Wallpaper Switchr/Array_shuffle.swift
@@ -0,0 +1,18 @@
+//
+//  Array_shuffle.swift
+//  RSS Wallpaper Switchr
+//
+//  Created by 蔡依達 on 2015/6/10.
+//  Copyright (c) 2015年 I-Ta Tsai. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    mutating func shuffle() {
+        for i in 0..<(count - 1) {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            swap(&self[i], &self[j])
+        }
+    }
+}
